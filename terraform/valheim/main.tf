strayer/valheim-dedicated-server-deployment

commit 3e8f9aed2511184c11296bf306adc3810d8cf659

Update Terraform cloudflare to v4.30.0

terraform/valheim/main.tf
@@ -96,22 +96,6 @@ variable "ssh_pubkey" {
   sensitive = true
 }
 
-variable "game_persona_bot_name" {
-  type = string
-}
-
-variable "game_persona_bot_avatar_url" {
-  type = string
-}
-
-variable "bot_server_started_message" {
-  type = string
-}
-
-variable "bot_server_ready_message" {
-  type = string
-}
-
 # Configure the Hetzner Cloud Provider
 provider "hcloud" {
   token = var.hcloud_token
@@ -207,10 +191,6 @@ resource "hcloud_server" "valheim-server" {
     valheim_server_name                  = var.valheim_server_name,
     valheim_server_world                 = var.valheim_server_world,
     valheim_server_password              = var.valheim_server_password,
-    game_persona_bot_name                = var.game_persona_bot_name,
-    game_persona_bot_avatar_url          = var.game_persona_bot_avatar_url,
-    bot_server_started_message           = var.bot_server_started_message,
-    bot_server_ready_message             = var.bot_server_ready_message,
     valheim_discord_channel_webhook      = var.valheim_discord_channel_webhook
   })
 }

terraform/factorio/.terraform.lock.hcl
@@ -2,23 +2,38 @@
 # Manual edits may be lost in future updates.
 
 provider "registry.terraform.io/cloudflare/cloudflare" {
-  version     = "4.2.0"
+  version     = "4.30.0"
   constraints = "~> 4.0"
   hashes = [
-    "h1:/1FIMpJ9kmO0vOGfYLxAtw/wk/Y7E8z17knE/4Ld9Ec=",
-    "h1:/4eNh0oAMGr12QNOYoEIGBUOujrPCR10bE5O0ZfAixk=",
-    "h1:MaQj+s/ZNW6aw3CzomeeXrtL9Yx9S8hynNpTIdpqOrs=",
-    "h1:MfVwZZ6017SioK2Wwe7izbi2H4O0ktcmmaF/khwPZIk=",
-    "h1:QOcbDlGhCGPrmRuNUlXifoO8DaO1Um8rvVXg4PFEWpE=",
-    "h1:Yruzny4P0BwqSN3+OwbaIa+s0F/aj/V9sCfJIKULHa4=",
-    "h1:ef3BA8WRnMsvR0HWONeMBSZTqYDkjqiz7ZhsxTUr2b8=",
-    "h1:fflKopUCZa62UVxnSgvkNqkDPmk5wNAnYIf61FraTys=",
-    "h1:jQisoWWMFnsz2L9wD0G40IaxvOxrKohMZ2/wRhHozZk=",
-    "h1:jlJ0PGBY05yGzne9zvWScND/AuXXgec5D3appKPYag8=",
-    "h1:oj2ywLGNtMvGSjgmGZIz7+U9t9b2T309KToZ78T2a9k=",
-    "h1:rjwLjnBgMnxJCdIuMXfjpCl8mEvHDbPgMFkcvQzNIps=",
-    "h1:scClDtHuU0P4rQ7hli91vvC+F7J6sZm83/kvnBH8gGs=",
-    "h1:zqDcpjljtpfIYPSO68rrdjifTRSzyLsM7Aic8uSoEG0=",
+    "h1:+sw3nDmN439jp/wRUnxhYvmvy7lxAmaE/PxxruCpcAs=",
+    "h1:05UXM990Xso5MGGhB75CbiZnCs4pGNF+ov6vMXoKuDw=",
+    "h1:9f36r9vcStap6J2MBA1Sbn/HtIojcc++rFKEqr6ygVM=",
+    "h1:FhhTF09/BBk37akGLFx9/uWkGUGwSNRub8vP80TaF7Q=",
+    "h1:Fzjj14EChfjqCXwZwEHxn4S3W3AJie34Z1uiKl8pvtc=",
+    "h1:HMxD7OEehCSAr78ZwIPoOpdAGiAixLx15v7/JBmxgt8=",
+    "h1:HydbKJyE9yCIXqCuD6d+TxiPKsDc6UCj+5KKEQ+3DqI=",
+    "h1:Ne1ArJAaV45W0eOEYY1m/G8ILJ6MST1J1YsBX41iEtk=",
+    "h1:PwWXnsZymnpXIBxe1ZSGeaRJsIDuhxnL8Wouho4U6CE=",
+    "h1:RCa0TYsEy445PJKu3VfIWi4VtRPR5imc5/JLpeSXKkg=",
+    "h1:W/q4chfazm9sEz6PVB7K2Uow+RXjLGwZ5F+jTRPfu/k=",
+    "h1:b5YZo6YJ35sKxhGrGSt+LaVhFGHsfhYfLK11gcWvWh0=",
+    "h1:hfnb9phsW/dOrprh9D5qZDkFh0wLVWSkrwGtT8Xp2Ng=",
+    "h1:ktPiwC8grfTg7Tx+ZtuKk5ztrCvsVIwGZig1a3xHbWE=",
+    "zh:218d1948b59e3d2e3af082724a0d057bcca5a5643c5e7c3b85eefc02430edd6b",
+    "zh:24eb677bc1b205565efb5c0d1c464f63d1e240aac61f5b2ef15165fe842cb7e2",
+    "zh:27896ed2a4f05f6a46ef25e674e445e89bd4bfba8cddbe95940109c6dc3179cc",
+    "zh:38b3b8297a9650b0ed09d57e0d802f5d851062bdadf72825652232c9a67346ac",
+    "zh:58d49ec9f414d0ff71e94cc991e1e3e33a13502ce0fea1393edd1297d0877bab",
+    "zh:5ed92c556e72cc4ea7fdf6db9e0dd7b093d179e26f2d2989b21a004a6402f2ae",
+    "zh:71f5c64702a7b2102f6d5edfd767953cd5b1248093c05983b909de06cf0c40cc",
+    "zh:788a023967db63b8eda9c0415851a743daf4073bab66b0bd1204bccbb54c9f8f",
+    "zh:7b9cd30355b4f63941284998167c3f3e5d208685e5176928275436de012f62d2",
+    "zh:890df766e9b839623b1f0437355032a3c006226a6c200cd911e15ee1a9014e9f",
+    "zh:923ec04258fde407f0fce80488268f4277ffac68fb7240eee4f4373a344c5469",
+    "zh:97473bdb848a7f77832fde6d0e68877bdcc17bf47ae3639fb09e1aeff4a92a01",
+    "zh:9b8754d8f7c15878ecb8897a6ffc4e9ec95f4e5f0560f4129af82a8200e602ea",
+    "zh:b890723ed524d34e7fbee6c119714be23e1783b82441ce4c18871c9d54f10cbd",
+    "zh:c75e0e5f406653c9b4928d97a38410ad7bb20d48e260c17ae3125a77b0457bf5",
   ]
 }
 

terraform/valheim/.terraform.lock.hcl
@@ -2,27 +2,38 @@
 # Manual edits may be lost in future updates.
 
 provider "registry.terraform.io/cloudflare/cloudflare" {
-  version     = "4.21.0"
+  version     = "4.30.0"
   constraints = "~> 4.0"
   hashes = [
-    "h1:7x4XCK7/lAtlfb2B3o74KE3XPEUsMJ7+KtFEWfNkamU=",
-    "h1:V0s1nFszBTGtvhNUcAFS+yDZJqFebUN0hCwR5HakLho=",
-    "h1:gn0+26TFtCwrXn4TM/Kr5d6u4TnWYS0FDVAx8gwnDVs=",
-    "zh:35b80c29ba47dd843f4281903389a519e90406efe1fd440f704d3a8ccf5a8338",
-    "zh:5af1ff1d13c7e91cd7e5382000b8f25bdd437ae3b73895b5876eb556352baf65",
-    "zh:5cc5418817c766af16e2ca9f23ddf3bbdd3c7f5e1a65756ed6f010c75005493e",
-    "zh:61655486cf10f65367f2bdc53701edb95a068859d54d30050d5028f5028f762d",
-    "zh:6a6d09d78442b4177e768ddeecc2cd9807bea839ce660e6771df96ff33c34f10",
-    "zh:6e56c6db96fb87a3a150a28588aa8ed430ef165ca3fde9ad873d40fad1f19021",
-    "zh:72ab4b2ebc3e06d045b28fcf9156577c7c685fe8445154888aeda74a767b0666",
-    "zh:78aa9402a1dc8a1c545355a63f6f64a7585ac8cb85ea1f4ef2d63919b8ea9864",
-    "zh:7c337f94a1ebe35ca5aa8f36d11bea114a8baa09030036c3875c99595e6a3059",
+    "h1:+sw3nDmN439jp/wRUnxhYvmvy7lxAmaE/PxxruCpcAs=",
+    "h1:05UXM990Xso5MGGhB75CbiZnCs4pGNF+ov6vMXoKuDw=",
+    "h1:9f36r9vcStap6J2MBA1Sbn/HtIojcc++rFKEqr6ygVM=",
+    "h1:FhhTF09/BBk37akGLFx9/uWkGUGwSNRub8vP80TaF7Q=",
+    "h1:Fzjj14EChfjqCXwZwEHxn4S3W3AJie34Z1uiKl8pvtc=",
+    "h1:HMxD7OEehCSAr78ZwIPoOpdAGiAixLx15v7/JBmxgt8=",
+    "h1:HydbKJyE9yCIXqCuD6d+TxiPKsDc6UCj+5KKEQ+3DqI=",
+    "h1:Ne1ArJAaV45W0eOEYY1m/G8ILJ6MST1J1YsBX41iEtk=",
+    "h1:PwWXnsZymnpXIBxe1ZSGeaRJsIDuhxnL8Wouho4U6CE=",
+    "h1:RCa0TYsEy445PJKu3VfIWi4VtRPR5imc5/JLpeSXKkg=",
+    "h1:W/q4chfazm9sEz6PVB7K2Uow+RXjLGwZ5F+jTRPfu/k=",
+    "h1:b5YZo6YJ35sKxhGrGSt+LaVhFGHsfhYfLK11gcWvWh0=",
+    "h1:hfnb9phsW/dOrprh9D5qZDkFh0wLVWSkrwGtT8Xp2Ng=",
+    "h1:ktPiwC8grfTg7Tx+ZtuKk5ztrCvsVIwGZig1a3xHbWE=",
+    "zh:218d1948b59e3d2e3af082724a0d057bcca5a5643c5e7c3b85eefc02430edd6b",
+    "zh:24eb677bc1b205565efb5c0d1c464f63d1e240aac61f5b2ef15165fe842cb7e2",
+    "zh:27896ed2a4f05f6a46ef25e674e445e89bd4bfba8cddbe95940109c6dc3179cc",
+    "zh:38b3b8297a9650b0ed09d57e0d802f5d851062bdadf72825652232c9a67346ac",
+    "zh:58d49ec9f414d0ff71e94cc991e1e3e33a13502ce0fea1393edd1297d0877bab",
+    "zh:5ed92c556e72cc4ea7fdf6db9e0dd7b093d179e26f2d2989b21a004a6402f2ae",
+    "zh:71f5c64702a7b2102f6d5edfd767953cd5b1248093c05983b909de06cf0c40cc",
+    "zh:788a023967db63b8eda9c0415851a743daf4073bab66b0bd1204bccbb54c9f8f",
+    "zh:7b9cd30355b4f63941284998167c3f3e5d208685e5176928275436de012f62d2",
     "zh:890df766e9b839623b1f0437355032a3c006226a6c200cd911e15ee1a9014e9f",
-    "zh:91c8a229a39bb3b79766da6ad77bdd18afa1fadeb811129de64a8c40a15708eb",
-    "zh:a8cdad0dbea2528b716138d0e123ced7a676d24a785f7c27c14fe199f0e5d67a",
-    "zh:a982d06804e3abd4d50d09df3e6926253b43c86767b5d5fb69396ca479aecaff",
-    "zh:e1a2f880282aaa47344fda83c9f75dd3a317d152388adf1155c7aed3e12cb7a7",
-    "zh:e64bb1d0199d492535b0825527b01ca42039804cf3903f583976486c40d5328e",
+    "zh:923ec04258fde407f0fce80488268f4277ffac68fb7240eee4f4373a344c5469",
+    "zh:97473bdb848a7f77832fde6d0e68877bdcc17bf47ae3639fb09e1aeff4a92a01",
+    "zh:9b8754d8f7c15878ecb8897a6ffc4e9ec95f4e5f0560f4129af82a8200e602ea",
+    "zh:b890723ed524d34e7fbee6c119714be23e1783b82441ce4c18871c9d54f10cbd",
+    "zh:c75e0e5f406653c9b4928d97a38410ad7bb20d48e260c17ae3125a77b0457bf5",
   ]
 }
 

commit 0d9ba56dd2bd8dd99d7de999ac00462868e4657d

Fix main.tf broken by 3e8f9ae - wtf?

terraform/valheim/main.tf
@@ -96,6 +96,22 @@ variable "ssh_pubkey" {
   sensitive = true
 }
 
+variable "game_persona_bot_name" {
+  type = string
+}
+
+variable "game_persona_bot_avatar_url" {
+  type = string
+}
+
+variable "bot_server_started_message" {
+  type = string
+}
+
+variable "bot_server_ready_message" {
+  type = string
+}
+
 # Configure the Hetzner Cloud Provider
 provider "hcloud" {
   token = var.hcloud_token
@@ -192,6 +208,10 @@ resource "hcloud_server" "valheim-server" {
     valheim_server_world                 = var.valheim_server_world,
     valheim_server_password              = var.valheim_server_password,
     valheim_discord_channel_webhook      = var.valheim_discord_channel_webhook
+    game_persona_bot_name                = var.game_persona_bot_name,
+    game_persona_bot_avatar_url          = var.game_persona_bot_avatar_url,
+    bot_server_started_message           = var.bot_server_started_message,
+    bot_server_ready_message             = var.bot_server_ready_message,
   })
 }
 